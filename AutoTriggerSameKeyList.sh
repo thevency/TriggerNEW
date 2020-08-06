ed2675e27a08ec83366353fedfb3216bc275e102
Smi <huong.trinh@linecorp.com>
2020-08-06 14:49:51 +0700

    Fix 3

--- AutoTriggerSameKeyList.sh
+++ AutoTriggerSameKeyList.sh
@@ -2,14 +2,28 @@
 
 # Inventory should consist with ADSTYLE
 SERVER=$1
-OS="android"
-JOB_LIST="AOS_10.x_R58MC34H3PE_Note10 iOS_12.x_3cd794dc710325abb8881184106b1eb0fccb2099_XS"
-BRANCH="origin/ladm"
-PlanID_VALUE=""
-PHASE_VALUE="1"
-InventoryKey_list="lb.nYuXlQJ4blY lb.cNcwLKQbbNQ"
-ADSTYLE_LIST="Union_Image_1200x628"
-STATUS_TO_TEST=""
+OS=$2
+JOB_LIST=$3
+BRANCH=$4
+PlanID_VALUE=$5
+PHASE_VALUE=$6
+ADSTYLE_LIST=$7
+InventoryKey_list=$8
+STATUS_TO_TEST=$9
+
+echo "[AutoTriggerSameKey] Parameters"
+echo "[AutoTriggerSameKey] 1. SERVER: $SERVER"
+echo "[AutoTriggerSameKey] 2. OS: $OS"
+echo "[AutoTriggerSameKey] 3. JOB_LIST: $JOB_LIST"
+echo "[AutoTriggerSameKey] 4. BRANCH: $BRANCH"
+echo "[AutoTriggerSameKey] 5. PlanID_VALUE: $PlanID_VALUE"
+echo "[AutoTriggerSameKey] 6. PHASE_VALUE: $PHASE_VALUE"
+echo "[AutoTriggerSameKey] 7. ADSTYLE_LIST: $ADSTYLE_LIST"
+echo "[AutoTriggerSameKey] 8. InventoryKey_list: $InventoryKey_list"
+echo "[AutoTriggerSameKey] 9. STATUS_TO_TEST: $STATUS_TO_TEST"
+
+
+
 ADSTYLE_FILE="/Users/lw11996/.jenkins/adstyle_aos.properties"
 LOG_FILE="log_AutoTriggerSameListKey_`date +"%Y-%m-%d-%H:%M"`"
 #SERVER=http://10.254.194.36:8080/job/Jobs%20Base%20On%20Device/view/LADM/job
@@ -31,17 +45,19 @@ LOG_FILE="log_AutoTriggerSameListKey_`date +"%Y-%m-%d-%H:%M"`"
 
 #Only applied for Adstyles that have same inventory key list.
 for style in $ADSTYLE_LIST;do
-  echo "======================================\nTrigger Ad style: $style for all keys at `date +"%Y-%m-%d-%H:%M"`" >> $LOG_FILE.txt
-  echo "TestPlanID: $PlanID_VALUE\nAD_STYLE: $style" >> $LOG_FILE.txt
+  echo "======================================\nTrigger Ad style: $style for all keys at `date +"%Y-%m-%d-%H:%M"`" >> "$LOG_FILE".txt
+  # shellcheck disable=SC2086
+  echo "[AutoTriggerSameKey] TestPlanID: $PlanID_VALUE\n[AutoTriggerSameKey] AD_STYLE: $style" >> $LOG_FILE.txt
 
   for key in $InventoryKey_list;do
+    # shellcheck disable=SC2086
     echo "[AutoTriggerSameKey] Trigger for key: $key" >> $LOG_FILE.txt
 #    triggerList_AOS.sh: PlanID_VALUE=$1 STYLE_VALUE=$2 InventoryKey=$3 PHASE_VALUE=$4 JOB_LIST=$5 SERVER=$6 LOG=$7 $branch=$8
-    ./triggerList.sh $PlanID_VALUE $style $key $PHASE_VALUE "${JOB_LIST[@]}" $SERVER $LOG_FILE $BRANCH $OS $STATUS_TO_TEST
+    ./triggerList.sh $PlanID_VALUE $style $key $PHASE_VALUE "${JOB_LIST[@]}" "$SERVER" "$LOG_FILE" $BRANCH $OS $STATUS_TO_TEST
     sleep 120
   done
 
-  echo "[AutoTriggerSameKey] Finish Trigger Ad style $style" >> $LOG_FILE.txt
+  echo "[AutoTriggerSameKey] Finish Trigger Ad style $style" >> "$LOG_FILE".txt
 
 #================2. Add comment & Get Report Info ===========
 
@@ -80,7 +96,7 @@ for job in $JOB_LIST;do
   BUILD_LIST=()
 done
 
-echo "[AutoTriggerSameKey] Check For Ad style finish ..." >> $LOG_FILE.txt
+echo "[AutoTriggerSameKey] Check For Ad style finish ..." >> "$LOG_FILE".txt
 
 #============== Check AD STYLE FINISH ==========================
 #Phase 2: It is required to check if current Adstyle is finished.
@@ -91,19 +107,20 @@ echo "[AutoTriggerSameKey] Check For Ad style finish ..." >> $LOG_FILE.txt
     while [[ $STATUS_OF_ADSTYLE != "true" ]]
     do
       sleep 900
-      echo "[AutoTriggerSameKey] Waiting with cycle 15 minutes" >> $LOG_FILE.txt
+      echo "[AutoTriggerSameKey] Waiting with cycle 15 minutes" >> "$LOG_FILE".txt
     done
 
     if [[ $STATUS_OF_ADSTYLE == "true" ]]
     then
-      echo "[AutoTriggerSameKey] Trigger next style" >> $LOG_FILE.txt
+      echo "[AutoTriggerSameKey] Trigger next style" >> "$LOG_FILE".txt
+      # shellcheck disable=SC2086
       echo "[AutoTriggerSameKey] ==================" >> $LOG_FILE.txt
     else
-      echo "[AutoTriggerSameKey] May be a problem here - STATUS_OF_ADSTYLE: $STATUS_OF_ADSTYLE" >> $LOG_FILE.txt
+      echo "[AutoTriggerSameKey] May be a problem here - STATUS_OF_ADSTYLE: $STATUS_OF_ADSTYLE" >> "$LOG_FILE".txt
     fi
   else
     #Phase1: Wait until all job of current style finish
-    echo "[AutoTriggerSameKey] Phase 1 is trigger ...." >> $LOG_FILE.txt
+    echo "[AutoTriggerSameKey] Phase 1 is trigger ...." >> "$LOG_FILE".txt
 #    sleep 60
 #    # shellcheck disable=SC2039
 #    ./checkJobListFinish.sh "${JOB_LIST[@]}" $SERVER
---
+++ copyReport.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#========================================================================================#
+#   Copy Report To Folder:
+#   Happen For Each Release
+#   Input: Report Link File that listed all neccessary link of Report following Adstyle
+#
+#========================================================================================#
+
+
