f0de0d000e5379682b9cfc62b22c240c8907ea58
Smi <huong.trinh@linecorp.com>
2020-08-07 11:01:18 +0700

    Fix log

--- checkJobListFinish.sh
+++ checkJobListFinish.sh
@@ -7,21 +7,21 @@ SERVER=$2
 
 
 # shellcheck disable=SC2086
-echo "[CheckStatus] START"# >> $LOG_FILE.txt
+echo "[CheckStatus] START"
 
 for job in $JOB_LIST;do
 
   STATUS=`curl --silent $SERVER/$job/lastBuild/api/json|grep -E "result"|sed -E 's/.+result\"://g'|cut -d "," -f1`
 
-  echo "[CheckStatus] Job $job is $STATUS"# >> "$LOG_FILE".txt
+  echo "[CheckStatus] Job $job is $STATUS"
 
   while [[ $STATUS == "null" ]]
   do
-    echo "Waiting for cycle 10 minutes"# >> "$LOG_FILE".txt
+    echo "Waiting for cycle 10 minutes"
     sleep 600
   done
 
 done
-echo "[CheckStatus] All Jobs are DONE"# >> "$LOG_FILE".txt
-echo "[CheckStatus] END"# >> "$LOG_FILE".txt
+echo "[CheckStatus] All Jobs are DONE"
+echo "[CheckStatus] END"
 
