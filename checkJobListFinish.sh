94c24b59deca085bdcae560571269f15010e4cac
Smi <huong.trinh@linecorp.com>
2020-08-10 14:10:25 +0700

    fix listen to job

--- checkJobListFinish.sh
+++ checkJobListFinish.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 
-JOB_LIST=$1
-SERVER=$2
+#JOB_LIST=$1
+#SERVER=$2
+#FILE=$3 #store info about trigger
 #LOG_FILE=$3
-
+JOB_LIST="AOS_10.x_R58MC34H3PE_Note10 AOS_9.x_R58M87CHVDV_Note9"
+SERVER=http://10.254.194.36:8080/job/Jobs%20Base%20On%20Device/view/LADM/job
+FILE="info_2020-08-09-19_57"
 
 
 echo "[CheckStatus] START"
@@ -15,13 +18,27 @@ declare -i count=0
 
 for job in $JOB_LIST;do
 
-  STATUS=`curl --silent $SERVER/$job/lastBuild/api/json|grep -E "result"|sed -E 's/.+result\"://g'|cut -d "," -f1`
+  #Define what is last build of a job bases on Info File
+  BUILD_LIST=`grep -E "JOB-$job" $FILE.txt|sed -E 's/.+BUILD#//g'|cut -d "-" -f1`
+#  array_list="$IFS"
+  IFS=' ' read -r -a array <<< "$BUILD_LIST"
+
+  declare -i a=${#array[@]}
+
+  echo "[CheckStatus] Array List: "
+  # shellcheck disable=SC2145
+  echo "\t\t\t ${array[@]}"
+  echo "[CheckStatus] Size: $a"
+  lastBuild=${array[a-1]}
+  echo "[CheckStatus] Last Build of Job $job is $lastBuild"
+
+  STATUS=`curl --silent $SERVER/$job/$lastBuild/api/json|grep -E "result"|sed -E 's/.+result\"://g'|cut -d "," -f1`
 
   echo "[CheckStatus] Job $job is $STATUS"
 
-  while [[ $STATUS == "null" ]]
+  while [[ $STATUS == "null" || $STATUS = "" ]]
   do
-    sleep 120
+    sleep 180
     count=$((count+1))
     # shellcheck disable=SC2039
     if [[ $count == 3 ]]
@@ -29,7 +46,7 @@ for job in $JOB_LIST;do
       echo "Waiting for cycle 2 minutes"
       count=0
     fi
-    STATUS=`curl --silent $SERVER/$job/lastBuild/api/json|grep -E "result"|sed -E 's/.+result\"://g'|cut -d "," -f1`
+    STATUS=`curl --silent $SERVER/$job/$lastBuild/api/json|grep -E "result"|sed -E 's/.+result\"://g'|cut -d "," -f1`
   done
 
 done
